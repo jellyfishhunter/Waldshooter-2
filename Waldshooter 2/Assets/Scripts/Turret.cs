a7eca0eeedd70d70bdc69bcaea9826a48b992b4c
maltooo <ma.boedeker@gmail.com>
2016-04-23 20:58:29 +0200

    Canvas

--- Waldshooter 2/Assets/Scripts/Turret.cs
+++ Waldshooter 2/Assets/Scripts/Turret.cs
@@ -5,15 +5,16 @@ using System.Collections.Generic;
 public class Turret : MonoBehaviour {
 
 
-	public List<GameObject> enemysInTrigger;  
+	public List<Transform> enemysInTrigger;  
 
 	private Transform targetTransform; 
 	private enum States { attackEnemy, onHold};
+
 	float timeUntilnextShot = 0.5f; 
 	float attackIntervall = 0.5f; 
 	float range = 20f; 
 
-	int hp = 20; 
+	int hp = 100; 
 	int level = 1; 
 	int costs = 100; 
 	int upgradecosts = 50; 
@@ -23,26 +24,48 @@ public class Turret : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (myState == States.attackEnemy) {
-		
+			if(targetTransform != null)
+			face (targetTransform.position); 
 		}
 		if (myState == States.onHold) {
-			
+			face (targetTransform.position); 
 		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-			enemysInTrigger.Add (other.gameObject); 
+		if (other.gameObject.tag == "Enemy") {
+			enemysInTrigger.Add (other.transform); 
+		}
+			
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "Enemy") {
+			enemysInTrigger.Remove (other.transform); 
+		}
+
+	}
+		
+
+	public void face(Vector3 t)
+	{
+		Vector3 direction = (new Vector3(t.x, transform.position.y, t.z) - transform.position).normalized;
+		if (direction != Vector3.zero)
+		{
+			Quaternion lookRotation = Quaternion.LookRotation(direction);
+			transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+		}
 	}
 
 	public void hit(GameObject bullet)
 	{
-		hp -= bullet.GetComponent<Bullet>().hitValue;
+			hp -= bullet.GetComponent<Bullet>().hitValue;
 		if (hp <= 0)
 		{
 			die();
