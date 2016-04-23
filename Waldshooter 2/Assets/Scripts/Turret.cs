77ac00f1a4c511dd51b474fe405c8b28f1710935
maltooo <ma.boedeker@gmail.com>
2016-04-23 20:07:57 +0200

    Musik + Turnt

--- Waldshooter 2/Assets/Scripts/Turret.cs
+++ Waldshooter 2/Assets/Scripts/Turret.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; 
 
 public class Turret : MonoBehaviour {
 
-	public Transform bullet; 
+
+	public List<GameObject> enemysInTrigger;  
+
 	private Transform targetTransform; 
 	private enum States { attackEnemy, onHold};
 	float timeUntilnextShot = 0.5f; 
@@ -25,85 +28,16 @@ public class Turret : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (myState = States.attackEnemy) {
+		if (myState == States.attackEnemy) {
 		
 		}
-		if (myState = States.onHold) {
-		
+		if (myState == States.onHold) {
+			
 		}
 	}
 
-	Transform chooseTarget()
-	{
-
-		GameObject[] buildings = GameObject.FindGameObjectsWithTag("Building");
-
-		foreach (GameObject building in buildings)
-		{
-			if (inRangeOf(building))
-			{
-				return building.transform;
-			}
-		}
-
-		return GameObject.FindGameObjectWithTag("Base Tree").transform;
+	void OnTriggerEnter(Collider other) {
+			enemysInTrigger.Add (other.gameObject); 
 	}
 
-	bool inRangeOf(GameObject other)
-	{
-		return (Vector3.Distance(other.transform.position, transform.position) <= range);
-	}
-
-	void aimAndShoot()
-	{
-		// aim
-		face(targetTransform.position);
-
-		// range check
-		bool targetInRange = Vector3.Distance(targetTransform.position, transform.position) <= range;
-
-		// reload and shoot
-		if (timeUntilnextShot <= 0 && targetInRange)
-		{
-			shoot();
-			timeUntilnextShot = attackIntervall;
-			Debug.Log("shoot");
-		}
-
-		timeUntilnextShot -= Time.deltaTime;
-	}
-
-	void shoot ()
-	{
-		Vector3 bulletSpawnPosition = transform.position + transform.forward;
-		GameObject bulletInstance = Instantiate(bullet, bulletSpawnPosition, transform.rotation) as GameObject;
-		Bullet bulletScript = bulletInstance.GetComponent<Bullet>();
-		bulletScript.isPlayerBullet = false;
-		bulletScript.range = range;
-	}
-
-	public void hit(GameObject bullet)
-	{
-		hp -= bullet.GetComponent<Bullet>().hitValue;
-		if (hp <= 0)
-		{
-			die();
-		}
-	}
-
-	// TODO: enemy counter etc
-	void die()
-	{
-		Destroy(gameObject);
-	}
-
-	public void face(Vector3 t)
-	{
-		Vector3 direction = (new Vector3(t.x, transform.position.y, t.z) - transform.position).normalized;
-		if (direction != Vector3.zero)
-		{
-			Quaternion lookRotation = Quaternion.LookRotation(direction);
-			transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
-		}
-	}
 }
